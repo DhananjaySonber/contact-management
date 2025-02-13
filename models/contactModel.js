970570a741a8b3f52aca766a25d1caf713dbbd70
dhananjaysonber62 <139144680+DhananjaySonber@users.noreply.github.com>
2025-02-13 08:47:44 +0530

    Add files via upload

---
+++ models/contactModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    address: { type: String },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model("Contact", contactSchema);
