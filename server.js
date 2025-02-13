939fde07a8fbb180ec7ec44b9e645eb22df644b2
dhananjaysonber62 <139144680+DhananjaySonber@users.noreply.github.com>
2025-02-13 08:45:23 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const contactRoutes = require("./routes/contactRoutes");
+const errorHandler = require("./middlewares/errorMiddleware");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/contacts", contactRoutes);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
